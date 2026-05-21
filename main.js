c3b7b0c9947e5b78a177486da28a877eb829da8a
dinkyy2302 <dinkyykhurana23@gmail.com>
2026-05-21 07:08:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed=new Typed(".multiple-text",
+    {
+        strings:["Frontend Developer","UI/UX Designer","C++ Programmer","Joint Secretary-ISTE CU"],
+        typeSpeed:100,
+        backSpeed:100,
+        backDelay:1000,
+        loop:true,
+    }
+);
